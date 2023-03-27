8b6561177cc69d4760147e8fc59a21568e2d2f39
Kevin Barr <56774588+kevinbarr@users.noreply.github.com>
2023-03-27 16:03:53 -0700

    Update reset_mechanism.py
    skeleton code process

--- reset_mechanism.py
+++ reset_mechanism.py
@@ -1,1 +1,17 @@
+"""
+SPEED
 
+def goToPosition(position)
+  for 
+    # move
+
+while(check switch)
+  move down
+  sleep
+
+while true
+  while read ir
+  move to drop
+  sleep
+  move to wait
+"""
