2813ff2362598996be2697fb90a1fac17a174bd3
forbesashli <44144720+forbesashli@users.noreply.github.com>
2023-03-30 13:01:55 -0700

    documentation push

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# all the main control stuff
+"""Main Script
+
+This runs the main control loop for the wheel. Calls the homing, elevator, initialize and controll functions/files.
+
+"""
+
+#TODO: intilize board
+#TODO: Homing sequence
+#TODO: while true loop
