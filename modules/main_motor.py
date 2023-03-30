1c29aa348ea48c7dd47c305b73f44522e8bc5e5c
forbesashli <44144720+forbesashli@users.noreply.github.com>
2023-03-30 11:42:55 -0700

    initial structure push

---
+++ modules/main_motor.py
@@ -0,0 +1,39 @@
+#code for testing the main motor, high priority , med diffuculty
+
+from time import sleep
+import RPi.GPIO as gpio
+
+DIR = 6
+PWM = 13
+CW =1
+CCW =0
+
+gpio.setmode(gpio.BCM)
+gpio.setup(DIR, gpio.OUT)
+gpio.setup(PWM, gpio.OUT)
+gpio.output(DIR,CW)
+
+pi_pwm = gpio.PWM(PWM,500)		#create PWM instance with frequency
+pi_pwm.start(0)			
+# Main body of code
+try:
+    while True:
+        sleep(2)
+        gpio.output(DIR,CW)
+        for duty in range(0,101,1):
+            pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+            sleep(0.01) 
+        pi_pwm.ChangeDutyCycle(0) #breaking
+        sleep(5)
+
+        gpio.output(DIR,CCW)
+        for duty in range(0,101,1):
+            pi_pwm.ChangeDutyCycle(duty)
+            sleep(0.01)
+        sleep(5)
+
+              
+        
+except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+    print("Cleaning up!")
+    gpio.cleanup()
