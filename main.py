0cfd521f0d6786fbd94ae5f22a5ef1af16925e76
forbesashli <44144720+forbesashli@users.noreply.github.com>
2023-04-04 15:23:02 -0700

    new routine

--- main.py
+++ main.py
@@ -3,13 +3,22 @@
 This runs the main control loop for the wheel. Calls the homing, elevator, initialize and controll functions/files.
 
 """
-
+import RPi.GPIO as gpio
 from modules.initialize import init_board
+from modules.elevator_routine import homing_sequence, elevator_routine
 
 #TODO: intilize board
 #TODO: Homing sequence
 #TODO: while true loop
 
-
+SW = 22
 init_board()
+homing_sequence()
+
+while(True):
+    if(gpio.input(SW) == False):
+        elevator_routine()
+
+    else:
+        print("print no ball")
 
--- modules/elevator_routine.py
+++ modules/elevator_routine.py
@@ -18,15 +18,56 @@ functions:
 """
 
 #TODO: Update documentation
-
 import RPi.GPIO as gpio
-DIR = 20
-STEP = 21
+from time import sleep
+
 SW = 22
 
+DIR = 20
+STEP = 21
+CW =1
+CCW =0
+
+gpio.setmode(gpio.BCM)
+gpio.setup(DIR, gpio.OUT)
+gpio.setup(STEP, gpio.OUT)
+gpio.output(DIR,CW)
+
+gpio.setmode(gpio.BCM)
+gpio.setup(SW, gpio.IN)
+
 def homing_sequence():
-    while(gpio.input(SW)):
-        print("homing")
-#This will be called only when the IR sensor says there is a ball 
+
+    while(True):
+        gpio.output(DIR,CW) #2300
+        for x in range(50):
+            gpio.output(STEP,gpio.HIGH)
+            sleep(.00100)
+            gpio.output(STEP,gpio.LOW)
+            sleep(.00100)
+
+        if gpio.input(SW):
+            print("switch closed")
+            gpio.cleanup()
+            break
+
+def elevator_routine():
+    gpio.output(DIR,CCW)
+    for x in range(300): #2300
+            gpio.output(STEP,gpio.HIGH)
+            sleep(.000500)
+            gpio.output(STEP,gpio.LOW)
+            sleep(.000500)
+
+    gpio.output(DIR,CW) #2300
+    for x in range(300):
+            gpio.output(STEP,gpio.HIGH)
+            sleep(.000300)
+            gpio.output(STEP,gpio.LOW)
+            sleep(.000300)
+
+        
+#This will be called only wh
+# en the IR sensor says there is a ball 
 
 # do the routine for the stepper motor test, get to the top and then get to the bottom and wait to be called again 
